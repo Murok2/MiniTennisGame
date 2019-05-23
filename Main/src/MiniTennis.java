8893fce94231a96ef5efd13b017eb5414f9933a3
Murok2 <murok.maratbekkyzy@iaau.edu.kg>
2019-05-23 18:31:12 +0600

    Design

--- Main/src/MiniTennis.java
+++ Main/src/MiniTennis.java
@@ -1,4 +1,82 @@
-package PACKAGE_NAME;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;//с кнопками «Свернуть», «Во весь экран» и «Закрыть»). Оно может изменять размеры и перемещаться по экрану.
+import javax.swing.JPanel;// представляющий собой прямоугольное пространство, на котором можно размещать другие элементы
+import javax.swing.JOptionPane;
 
-public class MiniTennis {
+public class MiniTennis extends JPanel{
+    Racquet racquet = new Racquet(this);
+    Ball ball = new Ball(this);
+    int speed = 1;
+
+    private int getScore() {
+        return speed -1 ;
+    }
+    public MiniTennis() {
+        addKeyListener(new KeyListener() {
+            public void keyTyped(KeyEvent e) {
+
+            }
+            public void keyPressed(KeyEvent e) {
+                racquet.keyPressed(e);
+            }
+
+            public void keyReleased(KeyEvent e) {
+                racquet.keyReleased(e);
+            }
+        });
+        setFocusable(true); //не устанавливает фокус сам по себе, он просто дает возможность потенциально сосредоточиться на компонент
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        MainGame();
+    }
+    public static void MainGame() throws InterruptedException {
+        Music.Music("C:\\Users\\Murok\\mini-tennis-game\\Main\\music\\background.wav");
+        MiniTennis game = new MiniTennis();
+        JFrame frame = new JFrame();
+        frame.add(game);
+        frame.setSize(300, 400);
+        frame.setVisible(true);//makes the frame appear onscreen
+        frame.setBackground(Color.BLACK);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//The EXIT_ON_CLOSE operation exits the program when your user closes the frame
+        while (true) {
+            game.move();
+            game.repaint();
+            Thread.sleep(9);
+        }
+    }
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        ball.paint(g2d);
+        racquet.paint(g2d);
+
+        g2d.setColor(Color.BLACK);
+        g2d.setFont(new Font("Verdana", Font.BOLD, 30));
+        g2d.drawString(String.valueOf(getScore()), 10, 30);
+
+    }
+
+    private void move() {
+        ball.move();
+        racquet.move();
+    }
+
+    public void gameOver() {
+        int n = JOptionPane.showConfirmDialog(
+                this,
+                "Would you like to play again?",
+                "Game is end",
+                JOptionPane.YES_NO_OPTION);
+        if (n == JOptionPane.YES_OPTION) {
+            ball.yspd = -1;
+        } else {
+            System.exit(ABORT);
+        }
+    }
+
 }
--- Main/src/Racquet.java
+++ Main/src/Racquet.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Racquet {
+    private static final int Y = 350;
+    private static final int WIDTH = 80;
+    private static final int HEIGHT = 10;
+
+    private MiniTennis game;
+    private int x;
+    private int xspd;
+    public Racquet(MiniTennis game) {
+        this.game = game;
+    }
+
+    public void move() {
+        if (x + xspd > 0 && x + xspd < game.getWidth()- WIDTH)
+            x = x + xspd;
+    }
+
+    public void paint(Graphics2D g) {
+        g.setColor(Color.RED);
+        g.fillRect(x, Y, WIDTH, HEIGHT);
+    }
+
+    public void keyReleased(KeyEvent e) {
+        xspd = 0;
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT)
+            xspd = -game.speed;
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+            xspd = game.speed;
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, Y, WIDTH, HEIGHT);
+    }
+
+    public int getY() {
+        return Y;
+    }
 }
